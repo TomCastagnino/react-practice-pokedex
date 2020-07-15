43175e8b4e48d4386f489499adb8ef4043eee150
Tom Castagnino <tcastagnino@gmail.com>
2020-07-15 08:49:04 -0700

    Add Pokecard and Pokedex components

---
+++ src/Pokecard.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react'
+import './Pokecard.css';
+
+const POKE_API =  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+
+class Pokecard extends Component {
+
+    render() {
+        const props = this.props;
+        const img = `${POKE_API}${props.id}.png`;
+        return (
+            <div className='Pokecard'>
+                <h1>{props.name}</h1>
+                <img src={img} alt={props.name}/>
+                <div>Type: {props.type}</div>
+                <div>Exp: {props.exp}</div>
+                
+            </div>
+        );
+    }
+
+}
+
+export default Pokecard;
