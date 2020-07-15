69e13b45528c1215c6a4a45b62afb943e3e358aa
Tom Castagnino <tcastagnino@gmail.com>
2020-07-15 09:58:51 -0700

    Add pokegame component and final styling

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,19 +1,23 @@
 import React, {Component} from 'react'
 import './Pokecard.css';
 
-const POKE_API =  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+const POKE_API =  "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/";
+
+let padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3): number)
 
 class Pokecard extends Component {
 
     render() {
         const props = this.props;
-        const img = `${POKE_API}${props.id}.png`;
+        const img = `${POKE_API}${padToThree(props.id)}.png`;
         return (
             <div className='Pokecard'>
-                <h1>{props.name}</h1>
-                <img src={img} alt={props.name}/>
-                <div>Type: {props.type}</div>
-                <div>Exp: {props.exp}</div>
+                <h1 className='Pokecard-title'>{props.name}</h1>
+                <div className='Pokecard-img'>
+                    <img src={img} alt={props.name}/>
+                </div>
+                <div className='Pokecard-data'>Type: {props.type}</div>
+                <div className='Pokecard-data'>Exp: {props.exp}</div>
                 
             </div>
         );
